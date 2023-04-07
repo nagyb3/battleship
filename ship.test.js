de89b253273ba4270c9b6e2ddff3306bd1b1d519
nagyb3 <nagybence2003@gmail.com>
2023-04-07 21:35:02 +0200

    ship.test.js to use shipLength instead of length

--- ship.test.js
+++ ship.test.js
@@ -22,7 +22,7 @@ test('initialize a new horizontal Ship', () => {
     let newShip = new Ship(5, 3, 2);
     expect(newShip.x).toEqual(3)
     expect(newShip.y).toEqual(2)
-    expect(newShip.length).toEqual(5)
+    expect(newShip.shipLength).toEqual(5)
     expect(newShip.direction).toEqual('x')
 })
 
@@ -30,6 +30,6 @@ test('initialize a new vertical Ship', () => {
     let newShip = new Ship(5, 3, 2, 'y');
     expect(newShip.x).toEqual(3)
     expect(newShip.y).toEqual(2)
-    expect(newShip.length).toEqual(5)
+    expect(newShip.shipLength).toEqual(5)
     expect(newShip.direction).toEqual('y')
 })
